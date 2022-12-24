3a2cde75f81fba037d7ce3e46fb016b2afc386e7
ballpenman <qhfvpsdls@gmail.com>
2022-12-24 09:53:14 +0900

    캐릭터 이동을 관성으로 바꿈

--- BigProject/Assets/Scripts/PlayerController.cs
+++ BigProject/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69fbb1e1c89fea762cabcc187d68a19521d5087d749900fe869f00a0e620180d
-size 3575
+oid sha256:c6c94c2ca042e2d90446e9eefe8df6629e5202d3cd7d8b3890048e8add541e57
+size 3589
