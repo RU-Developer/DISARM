a3f8b8e9eb1b2c9848456aac15063c17fc80991f
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2022-12-31 16:36:29 +0900

    각 역할들 함수로 추출
    플레이어 조작이 관성을 이용하기 때문에 InputManager를 통해서 조작함에 어려움이 있음

--- BigProject/Assets/Scripts/PlayerController.cs
+++ BigProject/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5d84dc6cf21c74802b82fa27e35fa3ac50e3b9228583891fcb87cd5390c4968
-size 4377
+oid sha256:6caa084304a7b121e4d2d1abd252f459e5d25ced896defc866dfe087eeb4fa5b
+size 4444
