28ed42fc649e197221c0444bb6fd637643f617c5
ballpenman <qhfvpsdls@gmail.com>
2023-01-12 22:02:36 +0900

    약간만 수정
    그냥 checkLedge부분을 좀 바꿔야 될 것 같다.

--- BigProject/Assets/Scripts/PlayerController.cs
+++ BigProject/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa94c7263be41d85bfe9b44cff76e73b7f712f7ac3f1eca6b06dc4c3e5b10bc9
-size 5562
+oid sha256:d67ab2427248b1744f8e9d3c4b7575ff0d4405a3ecbe3eab87a6cebb0bca98f6
+size 5615
