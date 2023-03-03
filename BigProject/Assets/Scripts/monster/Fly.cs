f318b62957d1cae6346251c2330098625219f98a
ballpenman <qhfvpsdls@gmail.com>
2023-03-03 15:37:11 +0900

    몬스터 fly 추가

--- BigProject/Assets/Scripts/monster/Fly.cs
+++ BigProject/Assets/Scripts/monster/Fly.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e8bae633ed2896c4a329a63d6e343c6d3e0a78e9070fe4786a507ed2db06dd7
-size 1935
+oid sha256:4cf0d35ae637a34a49f07d40706c6296a43a4b10794defe8f72e3d6a7bc0843d
+size 1982
