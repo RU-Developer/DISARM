4d130f00ea4836161759f90096c001bd0ece2273
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-03-03 21:06:20 +0900

    Scene이 변경되었을 시 Init호출해주는 부분을 BaseScene Awake로 변경
    Scene이 변경되었을 시 Init호출해주는 부분을 BaseScene Awake로 변경
    Scene을 정의할 때마다 새로 Init()을 호출해줄 필요 없어짐

--- BigProject/Assets/Scripts/Scenes/BaseScene.cs
+++ BigProject/Assets/Scripts/Scenes/BaseScene.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:541b67eb7c140150f6c4f4b40e57bd8e82cfb1731b902b2478d22bd9e0c5e83e
-size 852
+oid sha256:f9aef598783ec8f3e08f84e6395dc35402e7ade87bc7105b6363c5514dd6c94b
+size 898
