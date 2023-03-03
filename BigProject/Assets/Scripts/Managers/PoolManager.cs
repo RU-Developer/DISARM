119116fd8e3f6a1714dff6e3ee98af0db873d3a5
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-03-03 20:35:09 +0900

    장면전환시 오브젝트 풀링된 부분 삭제되지 않는 버그 수정
    장면전환시 오브젝트 풀링된 부분 삭제되지 않는 버그 수정

--- BigProject/Assets/Scripts/Managers/PoolManager.cs
+++ BigProject/Assets/Scripts/Managers/PoolManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bef98ffe849c52d032f4b877a0eb1f91a1bdfe8de032d6024718d4668750501
-size 4358
+oid sha256:e748f66de91b236c15f43c6ff7e41776b632ac7053e27c4aba8350e9ec3d1440
+size 4373
