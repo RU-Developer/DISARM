7833d6d50cd116bce3fee76f04325e34e81594e9
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-03-09 23:32:54 +0900

    1, 1을 추가하는 것이 아닌 타일맵의 cellSize만큼 추가

--- BigProject/Assets/Scripts/Scenes/SampleScene.cs
+++ BigProject/Assets/Scripts/Scenes/SampleScene.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cfd17f243cea9514bd489f5d6da34b26e0a77b7a1acc1bb887cb5f66c1235b2
-size 4664
+oid sha256:730ff3e78ef9785543ec15b09ef1e575c145e07cfa7aa210048d2e3dab8657f2
+size 4655
