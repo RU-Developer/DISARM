910aebfc4b2b67906d4aaaf78e9ca8bb581a53e0
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-03-23 20:23:31 +0900

    HP바 로그 삭제

--- BigProject/Assets/Scripts/UI/SubItem/UI_Game_Hp.cs
+++ BigProject/Assets/Scripts/UI/SubItem/UI_Game_Hp.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92a0b2030af98fd36be9ca07eb9ede784ff16dda16173c872373d595c082d484
-size 2402
+oid sha256:fd4f9bd2280fd585413e58947618556141e550cc867d56aa57d2a2c86ceb1b18
+size 2328
