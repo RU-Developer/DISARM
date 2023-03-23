ed906bcd0f2710ba24edb07c69a52a0b50174010
ballpenman <qhfvpsdls@gmail.com>
2023-03-23 15:48:07 +0900

    캐릭터 움직임 관성 일부 수정

--- BigProject/Assets/Scripts/Controllers/PlayerController.cs
+++ BigProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e858c4c54e342b2d39b10619f6b4188577e6d9b0bb527d99b92b2fc9c48da199
-size 8296
+oid sha256:b2ad3f26da08162ef4dd047a290d0162500ea37035be2082f26be12f5d912f38
+size 8005
