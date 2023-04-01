5704e51dffd9799594789f7b54964f943cfb4acc
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-04-02 01:09:01 +0900

    Game UI Action 해제 추가
    UIManager의 CloseSceneUI를 통해 제거할 때 GameManager의 Action을 해제해주지 않는 문제 해결

--- BigProject/Assets/Scripts/UI/SubItem/UI_Game_Hp.cs
+++ BigProject/Assets/Scripts/UI/SubItem/UI_Game_Hp.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e7c084b2f6ff731d79c9bdcb853da929436cf63bd573fc8ea9a76e98f7e091c
-size 2721
+oid sha256:da6bd7b622086441915822dd321c9c8f9ddf503b71fd899951903bc94aea3d18
+size 2881
