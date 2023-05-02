a668d58195efec05f687de323fdd67a2ec63044c
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-05-02 13:41:57 +0900

    FLY 몬스터 로테이션 안움직이고 해당방향으로 움직이게 변경
    1.5초간 이동 및 자연스럽게 감속기능 추가

--- BigProject/Assets/Scripts/Contents/Monster/Fly.cs
+++ BigProject/Assets/Scripts/Contents/Monster/Fly.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39baad2c534f2b83841a159ddce220e93c7a9850a4918abea6c5af18dc337717
-size 1908
+oid sha256:dc6e1a6b2c2751b2aa92631636e0b0c2e4001177977ccb31c03a5585deb7e33d
+size 2727
