b63ac03436468c503b70d9a268862ef6c9496efc
ballpenman <qhfvpsdls@gmail.com>
2023-05-09 15:55:01 +0900

    변수값 변경

--- BigProject/Assets/Scripts/Controllers/PlayerController.cs
+++ BigProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:743b9f7f25e81583e98180c459013c774e282b3a23d95107c6460d6068a95f4a
-size 8005
+oid sha256:6f551ed26b9423cde5febf9594453c3d6f7bfe32f4cbe58199ecd45e98d5b308
+size 8016
