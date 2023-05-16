4832d09e76c7c175c9b49535924c8c895d19389f
ballpenman <qhfvpsdls@gmail.com>
2023-05-17 03:16:37 +0900

    미끄러지는 경사로

--- BigProject/Assets/Scripts/Controllers/PlayerController.cs
+++ BigProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddcee781f7de52d989b3f8e0a4ea12aac0f0d9984dc021f05b1bb19ffa7d3515
-size 8331
+oid sha256:6fd1a986a8b8ed03deb69ea218388cd864b045863c3c7ab82bad40cb43114129
+size 9067
