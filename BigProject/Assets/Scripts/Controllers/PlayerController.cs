ec5c9158b3fdbfa1c054c325eb3ce391a983a7f5
ballpenman <qhfvpsdls@gmail.com>
2023-05-17 01:02:15 +0900

    몇몇 버그 수정 & 변수 수정

--- BigProject/Assets/Scripts/Controllers/PlayerController.cs
+++ BigProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c068499bf704a19ee5c1b63bc1bfa513075dedcec2b62176d17d80d1c3721df
-size 8492
+oid sha256:ddcee781f7de52d989b3f8e0a4ea12aac0f0d9984dc021f05b1bb19ffa7d3515
+size 8331
