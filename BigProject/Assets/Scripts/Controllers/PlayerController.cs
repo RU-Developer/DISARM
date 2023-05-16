6b0880989185b19517230cad0ad68311beee1062
ballpenman <qhfvpsdls@gmail.com>
2023-05-16 22:42:35 +0900

    파티클 추가

--- BigProject/Assets/Scripts/Controllers/PlayerController.cs
+++ BigProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f551ed26b9423cde5febf9594453c3d6f7bfe32f4cbe58199ecd45e98d5b308
-size 8016
+oid sha256:6c068499bf704a19ee5c1b63bc1bfa513075dedcec2b62176d17d80d1c3721df
+size 8492
