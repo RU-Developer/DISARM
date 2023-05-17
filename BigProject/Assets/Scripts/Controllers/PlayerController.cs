a965388eaefc83e5e44feadf1443e12ad67354a4
ballpenman <qhfvpsdls@gmail.com>
2023-05-17 12:28:49 +0900

    버그 고침

--- BigProject/Assets/Scripts/Controllers/PlayerController.cs
+++ BigProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ce878291389a547015ca62d52c15975ac035c76bdf622e80f200839a633290d
-size 9100
+oid sha256:d7b56e3ffca5949cc18dfaf57f27c79bc12ed82a109d67b1e8d8b2636f52e750
+size 9151
