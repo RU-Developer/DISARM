a5164ac2b2c223700f7eaf84dd06bc4d1f2996c9
ballpenman <qhfvpsdls@gmail.com>
2023-06-29 19:55:22 +0900

    모션 작업 1/2 완료

--- BigProject/Assets/Scripts/Controllers/PlayerController.cs
+++ BigProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7b56e3ffca5949cc18dfaf57f27c79bc12ed82a109d67b1e8d8b2636f52e750
-size 9151
+oid sha256:a6c39755c9e2d2a1dabbf0c4c54702f8f012e24d8610b5a6bff053f0947c9aa8
+size 10418
