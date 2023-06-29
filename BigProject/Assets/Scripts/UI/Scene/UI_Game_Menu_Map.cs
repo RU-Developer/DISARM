611ec4846a1100f2edefa13fb6a4581c13ef2b26
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-06-30 02:28:09 +0900

    아이콘을 맵 위에 출력시키는 기능까지는 만듬
    추후 아이콘을 json을 기반으로 받아서 넣어줄 예정 $"sprites/{path}"이런식으로 아이콘을 생성해서 넣어줄 예정

--- BigProject/Assets/Scripts/UI/Scene/UI_Game_Menu_Map.cs
+++ BigProject/Assets/Scripts/UI/Scene/UI_Game_Menu_Map.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9de862c196b86a0928607e785bced64ba4a8a9b7a268fb993f005d27da6bc918
-size 3784
+oid sha256:7f8674d5c67e263d757aee8be3ae8041a8f09f1d2b0dcdc40b58f5a20b656235
+size 5250
