377a14b64365e349cfd71869904e79556b3f7970
ballpenman <qhfvpsdls@gmail.com>
2023-07-01 13:07:54 +0900

    몬스터 추가

--- BigProject/Assets/Scripts/Contents/Monster/Walker.cs
+++ BigProject/Assets/Scripts/Contents/Monster/Walker.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b6b2fba5ab78826dceaabdbe5e95c861e8fb7badc7908c66d93e79039327db7
-size 1770
+oid sha256:b134fa3832647e35b81a0ff040dce639a38c940f89e7f7ec766b358f92c16d00
+size 3927
