7b80a94968acb2642dc77c82236b2c75b18b54e2
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-07-04 20:37:45 +0900

    투사체가 제거되서 터렛한테 맞으면 nullpointer 발생
    투사체가 제거되서 터렛한테 맞으면 nullpointer 발생
    log 남기는거 제거함

--- BigProject/Assets/Scripts/Contents/Status/Status.cs
+++ BigProject/Assets/Scripts/Contents/Status/Status.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca7ebc808ddef1b389e10ad2b9c27e3ee64cf6c61ecf4f99ad5c59c607841634
-size 2092
+oid sha256:fa26ff9085f0a3f6364dca7b6d8efaeb0c99f35b04b92ea707a3ed24548588fa
+size 2022
