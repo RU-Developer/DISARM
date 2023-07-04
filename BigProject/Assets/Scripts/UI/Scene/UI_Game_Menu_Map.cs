d46da6bd204b8157f7fbdfdb4c96ad978a928c62
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-07-05 01:50:50 +0900

    Map 이미지 sliced 적용
    Map 이미지 sliced 적용

--- BigProject/Assets/Scripts/UI/Scene/UI_Game_Menu_Map.cs
+++ BigProject/Assets/Scripts/UI/Scene/UI_Game_Menu_Map.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f652c7d266af57ed247d910b97d290dfd8f1d4159f469e61b2ddc4631022691a
-size 9587
+oid sha256:9808b8cf088d67bbffa622dd2f68792f0d5f3db57b4934929e9caf7262358c05
+size 9636
