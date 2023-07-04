ca63e0a6f0fad9cea39b3078b6bc5aa3245e2005
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-07-04 17:04:45 +0900

    HP Bar UI 적용완료
    HP Bar UI 적용완료

--- BigProject/Assets/Scripts/UI/SubItem/UI_Hp_Bar.cs
+++ BigProject/Assets/Scripts/UI/SubItem/UI_Hp_Bar.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:818049efbda4352746093dfed1ce5c6c7d638fc458b25733b5650ddc5df0b96b
-size 543
+oid sha256:74db3731150f565a118dd77829961c726071ecc5eba4ca68c768b82a547c0c5c
+size 556
