3e8e644c60a277acea8cdd29a9ad20f58ac27bfb
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-07-06 00:16:37 +0900

    Path 데이터의 type 정보에 따라 trigger를 통해 락 구현
    Path 데이터의 type 정보에 따라 trigger를 통해 락 구현

--- BigProject/Assets/Scripts/Camera/CameraSection.cs
+++ BigProject/Assets/Scripts/Camera/CameraSection.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2086481f10b6524f41ac9cb1d8c3f834dd29d64dc12dfd57051bae215d7d99f3
-size 1427
+oid sha256:9ca7c2e9745fcc897be2687d4b78a7ea94fb1eb4dc7b1d970de3ab1b9e4b9bcb
+size 2731
