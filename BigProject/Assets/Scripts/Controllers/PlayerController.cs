11c13a9ae35c8f0478f675618e3475cb34dea882
ballpenman <qhfvpsdls@gmail.com>
2023-07-05 12:38:50 +0900

    playerController수정

--- BigProject/Assets/Scripts/Controllers/PlayerController.cs
+++ BigProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e6d1a02e8bb56f68e876c75df804f615c49a2424458a8a0486b3b96c32b8291
+oid sha256:4c7482cd34cb67021d306bd2af3e2444a923402dcf857fa1f382e270dceefb39
 size 10582
