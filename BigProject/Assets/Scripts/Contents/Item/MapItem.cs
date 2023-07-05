ee51b98581f8f093d0dc961f2657603878a95ac5
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-07-05 15:54:05 +0900

    맵 아이템 정보 저장 부분 수정
    맵 아이템 정보 저장 부분 수정

--- BigProject/Assets/Scripts/Contents/Item/MapItem.cs
+++ BigProject/Assets/Scripts/Contents/Item/MapItem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d5512a60e1a902db769233d7c585aefa8b411659982449b00d6443f4f804947
-size 3495
+oid sha256:4e6088d3b53fde77b8d82aaa66b382f2213445f4f11cf4fc8d63d98dcd60a9ec
+size 3550
