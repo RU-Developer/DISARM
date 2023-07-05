7901630472d76b5ad45b6ced4fd1c6b56d8603a5
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-07-05 23:05:22 +0900

    쿨타임을 추가로 입력하여 사용 가능한 함수 추가
    쿨타임을 추가로 입력하여 사용 가능한 함수 추가

--- BigProject/Assets/Scripts/Managers/Contents/SkillManager.cs
+++ BigProject/Assets/Scripts/Managers/Contents/SkillManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fadc1a10dd9d71a7d99c7677f2acf2909d42af69ba0e084f91773d1304fef24
-size 1293
+oid sha256:6314d6da9d5883f2c1848acda0612ae3b2d17665a55777a754fa3f1c298c8528
+size 1693
