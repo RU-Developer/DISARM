ab1acd9b6ab4b0a43d3b8df3eb13cf217e7bb115
RU-Developer <61056453+RU-Developer@users.noreply.github.com>
2023-07-06 12:02:22 +0900

    Toast Message 오류 수정
    Toast Message 오류 수정

--- BigProject/Assets/Scripts/Managers/Core/UIManager.cs
+++ BigProject/Assets/Scripts/Managers/Core/UIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8b0176040f88aabbbde63521333dfcf409b0c9b4ea0e6c714749ef57405a83b
-size 5000
+oid sha256:a240f984c25595dcafe4c552777480e239f9979433a26bed59f2dfddb1c2f1b0
+size 4785
--- BigProject/Assets/Scripts/UI/Popup/UI_Toast_Message.cs
+++ BigProject/Assets/Scripts/UI/Popup/UI_Toast_Message.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:777bdc438312bf92cbfdfeca612a920092c9a29c0e35790c39b3bd6e8cfed7ce
-size 1543
+oid sha256:d435ada875178c29726b51d15c1a85d2ab1e9e42140386508216ef5ed89126ab
+size 1520
