415f041bc851eaecbf4733ae851a5e27030cd761
ballpenman <qhfvpsdls@gmail.com>
2023-07-06 10:00:20 +0900

    토스트메시지, 효과음 추가

--- BigProject/Assets/Scripts/Contents/Monster/Walker.cs
+++ BigProject/Assets/Scripts/Contents/Monster/Walker.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:963e35f968f3b8b61b6f45d2e6a097cdf5d5d1dae972fed5ca8b573a49377a17
-size 3858
+oid sha256:bb22de20477418d60be3f9709343da42e2eb47bf35a48db872bdfd0bfa0b1886
+size 3875
