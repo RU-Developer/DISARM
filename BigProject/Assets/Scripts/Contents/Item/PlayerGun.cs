b4d9eb5e5e02100e3a0664f1a8cf3580d03037a9
ballpenman <qhfvpsdls@gmail.com>
2023-09-26 03:27:27 +0900

    플레이어 모션 개편
    추가적으로 더 다듬을 예정

--- BigProject/Assets/Scripts/Contents/Item/PlayerGun.cs
+++ BigProject/Assets/Scripts/Contents/Item/PlayerGun.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f33bbe942adcad8c0ad196c0f8429a24252705af5e559b835aa20de6114a068
-size 8341
+oid sha256:33d3a2143c77bfe24e5672e6934b6f9aaf6180c527c6f361d2a07dbd8bb79d9b
+size 8042
--- BigProject/Assets/Scripts/Controllers/PlayerController.cs
+++ BigProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83b643d91c833f3a6bfffbf3aa54c2f5e4de2595137c3d14c33a2dc470185ddc
-size 15461
+oid sha256:8e4a9c04867bd6ced8b2ced0214dac40243dc09332c4c5047597d7ee53385e33
+size 16508
